f488ca8ecffecda6c5379975bc30d21bc456675f
ChouWiBaka <valentin.provost.pro@gmail.com>
2018-01-08 14:47:06 +0100

    Formulaire devis créé

---
+++ src/MaderaDevisBundle/Form/DevisType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MaderaDevisBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DevisType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('modele')
+            ->add('gamme')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MaderaDevisBundle\Entity\Devis'
+        ));
+    }
+}
